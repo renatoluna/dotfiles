3da4fba0cc6d68c4c3c4f09e9cec92e789bdee94
Renato Lopes de Luna Freire <renatoluna@gmail.com>
2014-09-06 23:50:03 -0300

    Adding snipmate

--- vimfiles/installPlugins.sh
+++ vimfiles/installPlugins.sh
@@ -4,3 +4,7 @@ cd ~/.vim/bundle;
 git clone https://github.com/mattn/emmet-vim.git;
 git clone https://github.com/terryma/vim-multiple-cursors;
 cd;
+rm -rf snipmate.vim;
+git clone https://github.com/msanders/snipmate.vim.git;
+cd snipmate.vim; cp -R * ~/.vim;
+cd;
