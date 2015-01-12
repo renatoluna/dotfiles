0cd48128185dc522ec767ed761cab699cb576b04
Renato Lopes de Luna Freire <renatoluna@gmail.com>
2015-01-12 13:54:01 -0200

    Adding Vim Git Gutter

--- vimfiles/installPlugins.sh
+++ vimfiles/installPlugins.sh
@@ -3,6 +3,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim;
 cd ~/.vim/bundle;
 git clone https://github.com/mattn/emmet-vim.git;
 git clone https://github.com/terryma/vim-multiple-cursors;
+git clone git://github.com/airblade/vim-gitgutter.git
 cd;
 rm -rf snipmate.vim;
 git clone https://github.com/msanders/snipmate.vim.git;
