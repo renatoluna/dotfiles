710829746e961d3445f87335e6ed5615ad3f731d
Renato Lopes de Luna Freire <renatoluna@gmail.com>
2016-02-03 15:23:47 -0200

    updating vim plugins

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
 cd ~
 git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s ~/Pessoal/dotfiles/vimfiles/.vimrc .
+ln -s ~/Pessoal/dotfiles/vimfiles/snippets/ .
