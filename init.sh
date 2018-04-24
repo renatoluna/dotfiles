1956a7b4d502a617142020cbf579022a159e281d
Renato Luna <renatoluna@gmail.com>
2018-04-24 14:55:24 -0300

    Updating init.sh
    Adding .bash_profile and .bash_aliases symbolic links

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@ git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/Pessoal/dotfiles/vimfiles/.vimrc .
 ln -s ~/Pessoal/dotfiles/vimfiles/snippets/ .
+ln -fs ~/Pessoal/dotfiles/.bash_profile .
+ln -fs ~/Pessoal/dotfiles/.bash_aliases .
